08ba0b123de312c733e8af0b3425beac8680e230
Ryan Meyers <rhynogolfer42@gmail.com>
2023-09-11 20:42:04 -0600

    Adds myfile.md

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,8 @@
-def parrot():
-    pass
+# The parrot() function should accept an argument of a string and both print() and 
+# return the string at the end of the function.
+
+# The parrot() function should have a default argument of "Squawk!"
+
+def parrot(squawk="Squawk!"):
+    print(squawk)
+    return squawk
